adbea26cfdad56dffe1f364eab08901ab3a5eb77
TuNa260998 <260998.tuna>
2024-06-22 15:16:39 +0700

    update automate file

--- automate-create-ptbxl-folders.sh
+++ automate-create-ptbxl-folders.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
 echo "Creating 22 folders of ptb-xl..."
-# Define the list of inputs
-inputs=($(printf "%05d " {0000..21000..1000}))  # Generates inputs from "01000" to "21000"
 
-# Loop through each input
-for input in "${inputs[@]}"
-do
-    # Run the command for each input
+# Loop to generate numbers from 00000 to 21000 in steps of 1000
+for (( num=0; num<=21000; num+=1000 )); do
+    input=$(printf "%05d" "$num")
     python prepare_ptbxl_data.py -i "ptb-xl/records100/$input" -d "ptb-xl/ptbxl_database.csv" -s "ptb-xl/scp_statements.csv" -o "$input"
 done
 
